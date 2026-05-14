7b956a98fd40c8a74400f973047f9d6bc870f3d4
M. Pramana Nugraha <pramana23ti@mahasiswa.pcr.ac.id>
2026-05-15 04:56:03 +0700

    Delete Custom Cursor because Lagging

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,14 @@ import LandingPage   from "./pages/LandingPage";
 import Dashboard     from "./pages/Dashboard";
 import FormPage      from "./pages/FormPage";
 import SplashScreen  from "./components/SplashScreen";
-import CustomCursor  from "./components/CustomCursor";
+
 
 export default function App() {
   const [splashDone, setSplashDone] = useState(false);
 
   return (
     <>
-      <CustomCursor />
+
       {!splashDone && <SplashScreen onDone={() => setSplashDone(true)} />}
       <BrowserRouter>
         <Routes>
